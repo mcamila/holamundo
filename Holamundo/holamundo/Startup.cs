16850b9706b5dc881d480bb19643b044f9acd8d7
mcamila <soporte2@sadmin.net>
2015-03-18 08:18:21 -0500

    Primeros cambios en el proyecto

---
+++ Holamundo/holamundo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(holamundo.Startup))]
+namespace holamundo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
